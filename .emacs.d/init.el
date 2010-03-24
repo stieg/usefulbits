c8ee685862d45eda1fda06674b31121da1380023
Andrew Stiegmann (stieg) <andrew.stiegmann@gmail.com>
2010-03-23 22:39:23 -0700

    started creating new emacs file

---
+++ .emacs.d/init.el
@@ -0,0 +1,15 @@
+;; ========== Enable Line and Column Numbering ==========
+
+;; Show line-number in the mode line
+(setq-default line-number-mode 1)
+
+;; Show column-number in the mode line
+(setq-default column-number-mode 1)
+
+; ========== Support Wheel Mouse Scrolling ==========
+
+(setq-default mouse-wheel-mode t)
+
+; === Disable Tabs ===
+
+(setq-default indent-tabs-mode nil) 
