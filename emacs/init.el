0dee49f33d8cbefa48f733cbb17e02a1490a3a88
Andrew Stiegmann (stieg) <andrew.stiegmann@gmail.com>
2016-01-04 16:40:09 -0600

    Add support for using package manager for plugins in emacs

--- emacs/init.el
+++ emacs/init.el
@@ -1,3 +1,10 @@
+;; Use the new Package manager.
+(require 'package)
+(package-initialize)
+
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+
+
 (add-to-list 'load-path "~/Devel/usefulbits/emacs/")
 (load "stieg.init.el")
-
